58fea52573d40974b6729f801d6fcce51b32f6f1
Grigorii Kargin <greg.kargin@gmail.com>
2018-08-03 01:33:34 +0300

    basic reader

--- src/MakeALisp/Expr.kt
+++ src/MakeALisp/Expr.kt
@@ -2,8 +2,18 @@ package MakeALisp
 
 interface Expr
 
-class EAtom() : Expr
+typealias Tokens = List<String>
+typealias Exprs  = List<Expr>
 
-class EList(val elements : List<Expr>) : Expr {
+interface EAtom : Expr
+class ENum(val value : Number) : EAtom
+class ESymbol(val value : String) : EAtom
 
+fun parseEAtom(token : String) : EAtom {
+    return when {
+        token.chars().allMatch(Character::isDigit) -> ENum(token.toInt())
+        else -> ESymbol(token)
+    }
 }
+
+class EList(val elements : List<Expr>) : Expr
--- src/MakeALisp/Reader.kt
+++ src/MakeALisp/Reader.kt
@@ -2,29 +2,45 @@ package MakeALisp
 
 fun readString() {}
 
-fun tokenize(str : String) : List<String> {
+fun tokenize(str : String) : Tokens {
     val matcher = Regex("""[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)""")
-    return matcher.findAll(str, 0).map { e -> e.value }.toList()
+    return matcher.findAll(str, 0).map { e -> e.value.trim() }.toList()
 }
 
-fun readList(tokens : List<String>) : Expr {
-    return EAtom()
+fun readList(tokens : Tokens) : Pair<EList, Tokens> {
+
+    val acc : MutableList<Expr> = mutableListOf()
+    var tokens = tokens
+
+    while (true) {
+        val nextToken = tokens.first()
+        if (nextToken == ")") {
+            return Pair(EList(acc), tokens.drop(1))
+        } else {
+            val result = readExpr(tokens)
+            acc.add(result.first)
+            tokens = result.second
+        }
+    }
 }
 
-fun readAtom(tokens : List<String>) : Expr {
-    return EAtom()
+fun readAtom(tokens : Tokens) : Pair<EAtom, Tokens> {
+    return Pair(parseEAtom(tokens.first()), tokens.drop(1))
 }
 
-fun readForm(tokens : List<String>) : Expr {
+fun readExpr(tokens : Tokens) : Pair<Expr, Tokens> {
+    print(tokens.first() + (tokens.first() == "("))
     return if (tokens.first() == "(") {
-        readList(tokens)
+        readList(tokens.drop(1))
     } else {
         readAtom(tokens)
     }
 }
 
-
-
 fun main(args: Array<String>) {
-    val a = tokenize("(foo (bar (baz)))")
+    val tokens = tokenize("(foo (bar (baz)))")
+    val exprs = readExpr(tokens)
+
+    print(tokens)
+    print(exprs)
 }
